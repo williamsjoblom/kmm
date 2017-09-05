1364bc8ca2b9dc38ca4482e9e500e6479c5cd171
williamsjoblom <wllmsjblm@gmail.com>
2017-09-05 11:26:33 +0200

    initial commit

---
+++ embedded/motor/stepper.c
@@ -0,0 +1,22 @@
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void)
+{
+  /* set PORTB for output*/
+  DDRB = 0xFF;
+  
+  while (1) {
+    /* set PORTB.6 high */
+    PORTB = 0x20;
+    
+    _delay_ms(2000);
+    
+    /* set PORTB.6 low */
+    PORTB = 0x00;
+    
+    _delay_ms(2000);
+  }
+  
+  return 1;
+}
