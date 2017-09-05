b0666a7ee852a868546d9bde0b187904c0a2a93d
williamsjoblom <wllmsjblm@gmail.com>
2017-09-05 22:42:08 +0200

    Set indentation in all files to 4 spaces

--- embedded/motor/main.c
+++ embedded/motor/main.c
@@ -50,25 +50,25 @@ unsigned int motor2_remaining = 0;
  * Main.
  */
 int main() {
-  // Set PORTB as output.
-  DDRB = 0xFF;
+    // Set PORTB as output.
+    DDRB = 0xFF;
 
-  set_motor_speed(0, 100000);
+    set_motor_speed(0, 100000);
 
-  install_timer(TIMER_INTERVAL);
+    install_timer(TIMER_INTERVAL);
 
-  serial_init();
+    serial_init();
 
-  while(1) {
-    /*if (serial_rx_available()) {
-      PORTB |= _BV(1); // Turn on LED @ PB1
-      int in_byte = serial_read();
-      serial_write(in_byte);
-      PORTB &= 253U; // Turn off LED
-    }*/
-  }
+    while(1) {
+	/*if (serial_rx_available()) {
+	  PORTB |= _BV(1); // Turn on LED @ PB1
+	  int in_byte = serial_read();
+	  serial_write(in_byte);
+	  PORTB &= 253U; // Turn off LED
+	  }*/
+    }
 
-  return 1;
+    return 1;
 }
 
 /**
@@ -76,16 +76,16 @@ int main() {
  * (revolutions per second/1000)
  */
 void set_motor_speed(unsigned char motor, unsigned long speed) {
-  const unsigned long timer_freq = 1000000 / TIMER_INTERVAL;
-  unsigned long step_freq = speed*STEPS_PER_REVOLUTION;
+    const unsigned long timer_freq = 1000000 / TIMER_INTERVAL;
+    unsigned long step_freq = speed*STEPS_PER_REVOLUTION;
 
-  unsigned int ticks = (unsigned int) ((timer_freq*1000)/step_freq);
+    unsigned int ticks = (unsigned int) ((timer_freq*1000)/step_freq);
 
-  switch(motor) {
-  case 0: motor0_ticks = ticks; break;
-  case 1: motor1_ticks = ticks; break;
-  case 2: motor2_ticks = ticks; break;
-  }
+    switch(motor) {
+    case 0: motor0_ticks = ticks; break;
+    case 1: motor1_ticks = ticks; break;
+    case 2: motor2_ticks = ticks; break;
+    }
 }
 
 
@@ -93,35 +93,35 @@ void set_motor_speed(unsigned char motor, unsigned long speed) {
  * Timer interrupt.
  */
 ISR(TIMER1_COMPA_vect) {
-  motor0_remaining--;
-  motor1_remaining--;
-  motor2_remaining--;
-
-  if (motor0_ticks != UINT_MAX) {
     motor0_remaining--;
-    if (motor0_remaining == 0) {
-      motor0_remaining = motor0_ticks;
-      PORTB |= MOTOR0_STP;
-    }
-  }
-
-  if (motor1_ticks != UINT_MAX) {
     motor1_remaining--;
-    if (motor1_remaining == 0) {
-      motor1_remaining = motor1_ticks;
-      PORTB |= MOTOR1_STP;
-    }
-  }
-
-  if (motor2_ticks != UINT_MAX) {
     motor2_remaining--;
-    if (motor2_remaining == 0) {
-      motor2_remaining = motor2_ticks;
-      PORTB |= MOTOR2_STP;
+
+    if (motor0_ticks != UINT_MAX) {
+	motor0_remaining--;
+	if (motor0_remaining == 0) {
+	    motor0_remaining = motor0_ticks;
+	    PORTB |= MOTOR0_STP;
+	}
     }
-  }
 
-  _delay_us(5);
+    if (motor1_ticks != UINT_MAX) {
+	motor1_remaining--;
+	if (motor1_remaining == 0) {
+	    motor1_remaining = motor1_ticks;
+	    PORTB |= MOTOR1_STP;
+	}
+    }
 
-  PORTB = direction;
+    if (motor2_ticks != UINT_MAX) {
+	motor2_remaining--;
+	if (motor2_remaining == 0) {
+	    motor2_remaining = motor2_ticks;
+	    PORTB |= MOTOR2_STP;
+	}
+    }
+
+    _delay_us(5);
+
+    PORTB = direction;
 }
