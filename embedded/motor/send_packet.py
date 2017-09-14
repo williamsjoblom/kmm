fcbf10e95b15afcb5ff2be8af18a14b7a1655496
williamsjoblom <wllmsjblm@gmail.com>
2017-09-14 15:57:55 +0200

    Add possibility to send speed vector packages over UART

--- embedded/motor/send_packet.py
+++ embedded/motor/send_packet.py
@@ -2,21 +2,64 @@
 
 import sys, os, readline, argparse, struct, serial, time, ctypes
 
-END_OF_TRANSMISSION = chr(4)
 
-def read_uint16():
+def is_int16(i):
+    """
+    Does i fit in a 16 bit signed integer?
+    """
+    return abs(i) < int(2**16/2)
+
+
+def is_int16_vector(v):
+    """
+    Is v a three dimensional vector?
+    """
+    for i in v:
+        if not is_int16(i):
+            return False
+        
+    return True
+    
+
+def read_int16():
+    """
+    Read a 16 bit signed integer from stdin.
+    """
     while True:
         try:
             i = int(input('> '))
-            if abs(i) < int(2**16/2):
+            
+            if is_int16(i):
                 return i
         except ValueError:
             pass
+        
         print("Must be a positive integer between 0 and", 2**16/2 - 1)
 
+        
+def read_speed_vector():
+    """
+    Read three dimensional 16 bit signed integer vector from stdin.
+    """
+    while True:
+        try:
+            v = list(map(int, input('> ').strip().split(" ")))
+            
+            if len(v) == 3 and is_int16_vector(v):
+                return (v[0], v[1], v[2])
+        except ValueError:
+            pass
+        
+        print("Vector must be three positive integers between 0 and", 2**16/2 - 1)
+
+    
 
 def build_packet():
-    cmd = read_uint16()
+    """
+    Build packet bytearray from user input.
+    """
+    cmd = 0
+    speed_vector = read_speed_vector()
                                         
     buf = []
 
@@ -25,13 +68,14 @@ def build_packet():
     
     # Data
     buf.extend(struct.pack("<h", cmd))
-    for i in range(3):
-        buf.extend(struct.pack("<h", -i))
+    for i in speed_vector:
+        buf.extend(struct.pack("<h", i))
 
     # Magic 8-bit footer
     buf.extend(struct.pack("<b", 24))
 
     return bytearray(buf)
+
 
 if __name__ == "__main__":
     p = argparse.ArgumentParser()
