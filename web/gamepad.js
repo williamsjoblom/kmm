190ae7d4d2121d8c2afdbc30d37ee484528052ca
sabze <sabse455@student.liu.se>
2017-11-06 16:33:25 +0100

    added gamepad.js and added it to index.html

---
+++ web/gamepad.js
@@ -0,0 +1,43 @@
+// Boolean that is true if a gamepad is connected.
+var connected = false;
+// The current gamepad object
+var gamepad;
+
+/* Sets upp eventlisteners for when the gamepad gets connected and disconnected.
+Starts listening as soon as the page has loaded.
+*/
+$(document).ready(function() {
+  window.addEventListener("gamepadconnected", function(e) {
+    gamepadHandler(e, true)
+  }, false);
+  window.addEventListener("gamepaddisconnected", function(e) {
+      gamepadHandler(e, false);
+  }, false)
+  window.setInterval(checkMovement, 100);
+});
+
+/* Logs to the console if the gamepad gets connected/disconnected
+ and sets the connected boolean as approriate.
+*/
+function gamepadHandler(event, connecting){
+  gamepad = event.gamepad;
+  if (connecting){
+    console.log("Gamepad connected");
+    connected = true;
+  } else {
+    console.log("Gamepad disconnected");
+    connected = false;
+  }
+}
+
+function checkMovement(){
+  if (connected) {
+    var x = gamepad.axes[1];
+    var y = gamepad.axes[0];
+    var theta = gamepad.axes[2];
+    //TODO: check deadzone
+    //TODO: implement low pass filter
+    //TODO: Check how x,y, theta corresponds to the controller
+
+  }
+}
