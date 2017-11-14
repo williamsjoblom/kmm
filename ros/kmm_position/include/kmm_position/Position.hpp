916ee4b7829b9443078b86567133350f0b9d86e2
Erik Örjehag <erik@orjehag.se>
2017-11-14 21:33:09 +0100

    fuck tf

--- ros/kmm_position/include/kmm_position/Position.hpp
+++ ros/kmm_position/include/kmm_position/Position.hpp
@@ -3,6 +3,12 @@
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
 #include <Eigen/Dense>
+#include "kmm_position/Pose.h"
+#include <tf/transform_broadcaster.h>
+#include <tf/transform_listener.h>
+#include "tf/message_filter.h"
+#include "message_filters/subscriber.h"
+#include "laser_geometry/laser_geometry.h"
 
 namespace kmm_position {
 
@@ -11,13 +17,27 @@ public:
   Position(ros::NodeHandle nh);
   ~Position();
 
-  void laser_scan_callback(sensor_msgs::LaserScan msg);
+  void laser_scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg);
   void publish_points(std::string ns, std::vector<Eigen::Vector2f>& points, float r, float g, float b);
+  void broadcast_position(const ros::TimerEvent&);
 
 private:
   ros::NodeHandle nh_;
-  ros::Subscriber laser_scan_sub_;
+  ros::Timer broadcast_timer_;
+  laser_geometry::LaserProjection projector_;
+
+  // Subscribers
+  tf::TransformListener tf_listener_;
+  message_filters::Subscriber<sensor_msgs::LaserScan>* laser_sub_;
+  tf::MessageFilter<sensor_msgs::LaserScan>* laser_notifier_;
+
+  // Publishers
   ros::Publisher marker_pub_;
+  tf::TransformBroadcaster tf_broadcaster_;
+
+  // Position values
+  Pose lidar_messurement_;
+
 };
 
 }
--- ros/kmm_position/src/Position.cpp
+++ ros/kmm_position/src/Position.cpp
@@ -5,28 +5,42 @@
 namespace kmm_position {
 
   Position::Position(ros::NodeHandle nh)
-  : nh_(nh)
+  : nh_(nh),
+    lidar_messurement_(0.2, 0.2, 0)
   {
-    laser_scan_sub_ = nh_.subscribe("scan", 1, &Position::laser_scan_callback, this);
+    // Publishers
     marker_pub_ = nh_.advertise<visualization_msgs::Marker>("pair", 1);
+    broadcast_timer_ = nh_.createTimer(ros::Duration(0.01), &Position::broadcast_position, this);
+
+    // Subscribers
+    laser_sub_ = new message_filters::Subscriber<sensor_msgs::LaserScan>(nh_, "scan", 10);
+    laser_notifier_ = new tf::MessageFilter<sensor_msgs::LaserScan>(*laser_sub_, tf_listener_, "map", 10);
+    laser_notifier_->registerCallback(boost::bind(&Position::laser_scan_callback, this, _1));
+    laser_notifier_->setTolerance(ros::Duration(0.01));
   }
 
   Position::~Position() {
-
+    delete laser_sub_;
+    delete laser_notifier_;
   }
 
-  void Position::laser_scan_callback(sensor_msgs::LaserScan msg) {
-    ROS_INFO("GOT SCAN!");
+  void Position::laser_scan_callback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    // Convert scan in polar coordinates robot frame (base_link)
+    // to cartesian coordinates global frame (map).
+    sensor_msgs::PointCloud cloud;
+    try {
+      projector_.transformLaserScanToPointCloud("map", *msg, cloud, tf_listener_);
+    }
+    catch (tf::TransformException ex) {
+       ROS_WARN("%s", ex.what());
+       return;
+     }
 
+    // Create list of Eigen vectors.
     std::vector<Eigen::Vector2f> scan;
-    for (int i = 0; i < msg.ranges.size(); i++) {
-      float range = msg.ranges[i];
-      if (range > 0.16) {
-        float angle = i * 3.1415 / 180;
-        float x = cos(angle) * range + 0.2;
-        float y = sin(angle) * range + 0.2;
-        scan.push_back(Eigen::Vector2f(x, y));
-      }
+    scan.resize(cloud.points.size());
+    for (int i = 0; i < scan.size(); i++) {
+      scan.push_back(Eigen::Vector2f(cloud.points[i].x, cloud.points[i].y));
     }
 
     std::vector<Eigen::Vector2f> a, b;
@@ -39,7 +53,7 @@ namespace kmm_position {
   void Position::publish_points(std::string ns, std::vector<Eigen::Vector2f>& points, float r, float g, float b) {
     visualization_msgs::Marker markers;
     markers.type = visualization_msgs::Marker::POINTS;
-    markers.header.frame_id = "neato_laser";
+    markers.header.frame_id = "map";
     markers.ns = ns;
     markers.scale.x = 0.01;
     markers.scale.y = 0.01;
@@ -56,6 +70,16 @@ namespace kmm_position {
     }
 
     marker_pub_.publish(markers);
+  }
+
+  void Position::broadcast_position(const ros::TimerEvent&) {
+    tf::Vector3 position(lidar_messurement_.pos[0], lidar_messurement_.pos[1], 0);
+    tf::Quaternion orientation;
+    orientation.setEuler(0, 0, lidar_messurement_.angle);
+    tf_broadcaster_.sendTransform(
+      tf::StampedTransform(
+        tf::Transform(orientation, position),
+        ros::Time::now(), "map", "base_link"));
   }
 
 }
