541d8b6d0236ff9524ea2ee5f885104788088ca7
Jennifer Lindgren <jennifer.lindgren93@gmail.com>
2017-11-24 10:21:34 +0100

    Button node modifications

--- ros/kmm_drivers/nodes/kmm_input_node.py
+++ ros/kmm_drivers/nodes/kmm_input_node.py
@@ -5,7 +5,7 @@ import RPi.GPIO as GPIO
 from std_msgs.msg import Bool
 
 BTN_PIN = 11
-btn_state = false;
+btn_state = False;
 
 def callback(data):
     btn_state = data.data
@@ -21,10 +21,14 @@ if __name__ == '__main__':
         pub = rospy.Publisher('btn_state', Bool, queue_size=1)
         sub = rospy.Subscriber('btn_state', Bool, callback)
         rate = rospy.Rate(60) # 60Hz
+        pressed = False
 
         while not rospy.is_shutdown():
-            if GPIO.input(BTN_PIN):
+            if GPIO.input(BTN_PIN) and !pressed:
                 btn_state = !btn_state
+                pressed = True;
+            elif !GPIO.input(BTN_PIN):
+                pressed = False;
             pub.publish(btn_state)
             rate.sleep()
     except rospy.ROSInterruptException:
